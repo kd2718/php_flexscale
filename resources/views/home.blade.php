88c9628d7450ae9d22def73397e9d6e81454a59e
koryd <korydonati@gmail.com>
2019-06-19 23:22:33 -0700

    added database and made small changes to hoomepage and landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="row">
+        <div class="col-12">
+            <h3>Welcome Cheri</h3>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <div class="card border-primary">
+                  <img class="card-img-top" src="holder.js/100px180/" alt="">
+                  <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Text</p>
+                  </div>
                 </div>
+                <h4>Here is my Weight</h4>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
